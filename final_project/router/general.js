67e657d5266cdee3bb6d23cbb1be2099acfadae1
Harsha <vardhanharsha607@gmail.com>
2025-04-06 07:28:48 -0400

    added user registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,10 +5,25 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+  
+    // Check if username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+    }
+  
+    // Check if user already exists
+    const userExists = users.some(user => user.username === username);
+    if (userExists) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+  
+    // Add new user
+    users.push({ username, password });
+    return res.status(200).json({ message: "User registered successfully" });
+  });
+  
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
