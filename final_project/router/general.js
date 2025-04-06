a12df56430d99e406f6625465965deaa7babf68b
Harsha <vardhanharsha607@gmail.com>
2025-04-06 07:11:31 -0400

    added get books by tite

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,7 +51,20 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const booksByTitle = [];
+
+  for (let isbn in books) {
+    if (books[isbn].title.toLowerCase() === title) {
+      booksByTitle.push({ isbn, ...books[isbn] });
+    }
+  }
+
+  if (booksByTitle.length > 0) {
+    return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
+  } else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 //  Get book review
